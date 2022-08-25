2f5ed23538ad74b015a3ec5c75be88436bfda42e
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-25 16:53:24 -0300

    Loading product data

--- src/pages/product-detail/product-detail.ts
+++ src/pages/product-detail/product-detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProductDTO } from '../../models/product.dto';
+import { ProductService } from '../../services/domain/product.service';
 
 @IonicPage()
 @Component({
@@ -11,16 +13,28 @@ export class ProductDetailPage {
 
   item: ProductDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public productService: ProductService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductDetailPage');
-    this.item = {
-      id: "1",
-      name: "Mouse",
-      price: 80.59
-    }
+    let product_id = this.navParams.get('product_id');
+    this.productService.findById(product_id)
+    .subscribe(response => {
+      this.item = response;
+      this.getImageUrlIfExists();
+    },
+    error => {});
+  }
+
+  getImageUrlIfExists() {
+    this.productService.getImageFromBucket(this.item.id)
+    .subscribe(response => {
+      this.item.imgUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+    },
+    error => {})
   }
 
 }
--- src/services/domain/product.service.ts
+++ src/services/domain/product.service.ts
@@ -2,11 +2,16 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.config";
+import { ProductDTO } from "../../models/product.dto";
 
 @Injectable()
 export class ProductService {
 
     constructor(public http: HttpClient) {
+    }
+
+    findById(product_id : string) {
+        return this.http.get<ProductDTO>(`${API_CONFIG.baseUrl}/products/${product_id}`);
     }
 
     findByCategory(category_id : string) {
@@ -15,6 +20,11 @@ export class ProductService {
 
     getSmallImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType : 'blob'});
+    }
+
+    getImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
         return this.http.get(url, {responseType : 'blob'});
     }
 }
