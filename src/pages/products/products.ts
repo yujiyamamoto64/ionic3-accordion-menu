12b32a0ed6cc4e0927a0500066f7652f778cd40b
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-25 16:06:07 -0300

    Load product images

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProductDTO } from '../../models/product.dto';
 import { ProductService } from '../../services/domain/product.service';
 
@@ -23,8 +24,20 @@ export class ProductsPage {
     this.productService.findByCategory(category_id)
     .subscribe(response => {
       this.items = response['content'];
+      this.loadImageUrls();
     },
     error => {});
+  }
+
+  loadImageUrls() {
+    for (var i = 0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.productService.getSmallImageFromBucket(item.id)
+      .subscribe(response => {
+        item.imgUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+      },
+      error => {})
+    }
   }
 }
 
