d0e1282ad88b2287fa7aaa2bb154d63f27083a84
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-29 14:12:44 -0300

    Order success notification

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   client: ClientDTO;
   address: AddressDTO;
+  codorder: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -56,17 +57,26 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriesPage');
+  }
+
   checkout() {
     this.orderService.insert(this.order)
     .subscribe(response => {
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codorder = this.extractId(response.headers.get('location'));
     },
     error => {
       if (error.status == 403) {
         this.navCtrl.setRoot('HomePage');
       }
     });
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
